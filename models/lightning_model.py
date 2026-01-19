034fedc559cf201de94b5c3035486cfea6794895
Kishan Bhimani <58839585+MrBhimani@users.noreply.github.com>
2026-01-19 21:31:36 +0530

    Create lightning_model.py

---
+++ models/lightning_model.py
@@ -0,0 +1,40 @@
+#LightningModule
+import torch
+from torch import nn
+import pytorch_lightning as pl
+from torchmetrics import MeanSquaredError
+
+
+class StockPredictor(pl.LightningModule):
+    def __init__(self, input_dim: int, lr: float = 1e-3):
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.model = nn.Sequential(
+            nn.Linear(input_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, 1),
+        )
+
+        self.criterion = nn.MSELoss()
+        self.metric = MeanSquaredError()
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x).squeeze()
+        loss = self.criterion(y_hat, y)
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x).squeeze()
+        loss = self.criterion(y_hat, y)
+        self.log("val_loss", loss, prog_bar=True)
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
